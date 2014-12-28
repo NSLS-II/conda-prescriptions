da7acbb833f4bf24a2c70a4a864e8f791087f19f
Stuart Wilkins <swilkins@bnl.gov>
2014-12-28 11:54:26 -0500

    Fixed dependencies and git strings

--- epics-base/build.sh
+++ epics-base/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-make
+make -j32
 echo $PREFIX
-mkdir $PREFIX/lib
-cp -av lib/linux-x86_64/* $PREFIX/lib
-mkdir $PREFIX/bin
-cp -av bin/linux-x86_64/caget $PREFIX/bin
+EPICS_BASE=$PREFIX/lib/epics
 
+install -d $PREFIX/lib
+cp -Prv lib/linux-x86_64/* $PREFIX/lib
 
+install -d $PREFIX/bin
+cp -Prv bin/linux-x86_64/caget $PREFIX/bin
+cp -Prv bin/linux-x86_64/caput $PREFIX/bin
+cp -Prv bin/linux-x86_64/camonitor $PREFIX/bin
+cp -Prv bin/linux-x86_64/caRepeater $PREFIX/bin
+cp -Prv bin/linux-x86_64/softIoc $PREFIX/bin
+
+for X in bin configure db dbd include lib startup templates; do
+  install -d $EPICS_BASE/$X
+  cp -Prv $X/* $EPICS_BASE/$X
+done
