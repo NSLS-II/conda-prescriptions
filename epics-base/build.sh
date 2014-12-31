7ad62732c4cf871e7e4886ade7da5530c33165bb
Thomas A Caswell <tcaswell@bnl.gov>
2014-12-31 15:25:19 -0500

    BLD : drop manual installation
    Using the PREFIX patch, the files are built in the correct
    place so don't need to manually copy anything

--- epics-base/build.sh
+++ epics-base/build.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
 make -j$(getconf _NPROCESSORS_ONLN)
-EPICS_BASE=$PREFIX/lib/epics
-
-install -d $PREFIX/lib
-cp -Prv lib/linux-x86_64/* $PREFIX/lib
-
-install -d $PREFIX/bin
-cp -Prv bin/linux-x86_64/caget $PREFIX/bin
-cp -Prv bin/linux-x86_64/caput $PREFIX/bin
-cp -Prv bin/linux-x86_64/camonitor $PREFIX/bin
-cp -Prv bin/linux-x86_64/caRepeater $PREFIX/bin
-cp -Prv bin/linux-x86_64/softIoc $PREFIX/bin
-
-for X in bin configure db dbd include lib startup templates; do
-  install -d $EPICS_BASE/$X
-  cp -Prv $X/* $EPICS_BASE/$X
-done
