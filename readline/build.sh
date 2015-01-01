9c0eb7cd9cadd038a29d5f3dcbe40ee7d66c39cd
Stuart Wilkins <swilkins@bnl.gov>
2015-01-01 17:12:18 -0500

    Fixed to build correctly against tinfo not ncurses

--- readline/build.sh
+++ readline/build.sh
@@ -3,6 +3,6 @@
 set -e
 
 ./configure --prefix=$PREFIX --with-curses
-make -j$(getconf _NPROCESSORS_ONLN) SHLIB_LIBS="-lncurses -L$PREFIX/lib"
+make -j$(getconf _NPROCESSORS_ONLN) SHLIB_LIBS="-ltinfo -L$PREFIX/lib"
 make install
 
