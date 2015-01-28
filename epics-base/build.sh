96462146d774d65ac4009e840a26f640f66ff612
Stuart B. Wilkins <swilkins@bnl.gov>
2015-01-28 16:16:15 -0500

    BLD : Simplified symbolic link creation

--- epics-base/build.sh
+++ epics-base/build.sh
@@ -3,20 +3,15 @@ make -j$(getconf _NPROCESSORS_ONLN)
 EPICS_BASE=$PREFIX/lib/epics
 EPICS_HOST_ARCH=$(startup/EpicsHostArch)
 
-cd $PREFIX
 
 install -d $PREFIX/bin
 install -d $PREFIX/lib
-LIBDIR=$PREFIX/lib/epics/lib/$EPICS_HOST_ARCH
-BINDIR=$PREFIX/lib/epics/bin/$EPICS_HOST_ARCH
-LIBS=`find $LIBDIR/* -type f`
 BINS="caget caput camonitor softIoc"
 
-for file in $LIBS ; do
-	f=${file##$LIBDIR}
-	ln -s ../lib/epics/lib/$EPICS_HOST_ARCH/$f lib/$f
-done
+cd $PREFIX/lib
+ln -s epics/lib/$EPICS_HOST_ARCH/lib* .
+
+cd $PREFIX/bin
 for file in $BINS ; do
-	f=${file##$BINDIR}
-	ln -s ../lib/epics/bin/$EPICS_HOST_ARCH/$f bin/$f
+	ln -s ../lib/epics/bin/$EPICS_HOST_ARCH/$file .
 done
