9b33aebc2810b37ba9eb9e24335deac9d445f061
Stuart B. Wilkins <swilkins@bnl.gov>
2015-01-29 06:57:31 -0500

    BLD : Removed symlinks from lib directory.

--- epics-base/build.sh
+++ epics-base/build.sh
@@ -3,14 +3,14 @@ make -j$(getconf _NPROCESSORS_ONLN)
 EPICS_BASE=$PREFIX/lib/epics
 EPICS_HOST_ARCH=$(startup/EpicsHostArch)
 
-
 install -d $PREFIX/bin
 install -d $PREFIX/lib
+
+# Copy libraries into $PREFIX/lib
+cp -av $PREFIX/lib/epics/lib/$EPICS_HOST_ARCH/lib* $PREFIX/lib
+
+# Setup symlinks for utilities
 BINS="caget caput camonitor softIoc"
-
-cd $PREFIX/lib
-ln -s epics/lib/$EPICS_HOST_ARCH/lib* .
-
 cd $PREFIX/bin
 for file in $BINS ; do
 	ln -s ../lib/epics/bin/$EPICS_HOST_ARCH/$file .
