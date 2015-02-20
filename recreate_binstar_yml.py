9b740eca68f9919806e34239e5415494237bceeb
Eric Dill <edill@bnl.gov>
2015-02-20 15:38:13 -0500

    TOM: Tomify the function to use less global state

--- recreate_binstar_yml.py
+++ recreate_binstar_yml.py
@@ -4,8 +4,7 @@ import sys
 import re
 import yaml
 
-UPLOAD_ACCOUNT = None
-CHANNEL = None
+DEFAULT_CHANNEL = 'dev'
 
 def safe_yaml_read(fpath, replace_str=''):
     """
@@ -31,7 +30,11 @@ def safe_yaml_read(fpath, replace_str=''):
             lns.append(re.sub(r'{[{%].*?[%}]}', '', ln))
     meta_dict = yaml.load(''.join(lns))
     return meta_dict
-def make_template(package_name):
+
+
+def make_template(package_name, upload_acct, channel_name=None):
+    if channel_name is None:
+        channel_name = DEFAULT_CHANNEL
     binstar_yml_template = """## The package attribure specifies a binstar package namespace to build the package to.
 ## This can be specified here or on the command line
 package: {}
@@ -109,34 +112,42 @@ script:
 build_targets:
  files: conda
  channels: {}
-    """.format(package_name, UPLOAD_ACCOUNT, CHANNEL)
+    """.format(package_name, upload_acct, channel_name)
     return binstar_yml_template
 
-def write_yml(build_folder):
+
+def write_yml(build_folder, upload_acct, channel_name):
     meta_yaml_file = safe_yaml_read(os.path.join(build_folder, 'meta.yaml'))
     package_name = meta_yaml_file['package']['name']
     with open(os.path.join(build_folder, '.binstar.yml'), 'w') as f:
-        f.write(make_template(package_name))
+        f.write(make_template(package_name, upload_acct, channel_name))
 
 
-def make_ymls(root_folder):
+def make_ymls(root_folder, upload_acct, channel_name):
     if 'meta.yaml' in os.listdir(root_folder):
-        write_yml(root_folder)
+        write_yml(root_folder, upload_acct, channel_name)
     else:
         for subfolder in os.listdir(root_folder):
             subfolder = os.path.join(root_folder, subfolder)
             if os.path.isdir(subfolder):
-                make_ymls(subfolder)
+                make_ymls(subfolder, upload_acct, channel_name)
 
 
 if __name__ == "__main__":
-    print("Usage: python recreate_binstar_yml.py user_account channel_name")
-    print("Example: python recreate_binstar_yml.py edill main")
-    UPLOAD_ACCOUNT = sys.argv[1]
     try:
-        CHANNEL = sys.argv[2]
+        upload_acct = sys.argv[1]
+    except IndexError:
+        print("you supplied no command line arguments. I have no idea where to"
+              "upload your packages to. Please specify a user_account as the"
+              "first command line argument after 'recreate_binstar_yml.py'")
+        print("Usage: python recreate_binstar_yml.py user_account channel_name")
+        print("Example: python recreate_binstar_yml.py edill main")
+        sys.exit(1)
+
+    try:
+        channel_name = sys.argv[2]
     except IndexError:
         # use the default channel
-        CHANNEL = 'dev'
+        channel_name = None
     # make the yaml files
-    make_ymls(os.getcwd())
+    make_ymls(os.getcwd(), upload_acct, channel_name)
