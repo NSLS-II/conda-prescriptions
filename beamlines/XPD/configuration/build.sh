5786e6116b1f56deab9448a79b4627322e86c85a
Thomas A Caswell <tcaswell@bnl.gov>
2015-03-30 22:04:14 -0400

    ENH : add XPD meta + config packages

---
+++ beamlines/XPD/configuration/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p $PREFIX/etc
+ETC=$PREFIX/etc
+
+
+# set up
+echo "# mds configuration
+# installed by xpd_configuration
+# DO NOT EDIT
+host: xf28id-ca1.cs.nsls2.local
+database: datastore
+port: 27017
+timezone: US/Eastern
+" > $ETC/metadatastore.yml
+
+echo "# filestore configuration
+# installed by xpd_configuration
+# DO NOT EDIT
+host: xf28id-ca1.cs.nsls2.local
+database: filestore
+port: 27017
+" > $ETC/filestore.yml
+
+
+
+# clean up after self
+unset ETC
