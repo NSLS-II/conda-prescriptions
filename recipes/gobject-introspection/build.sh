459aa66679b30db3ea4b9e288dae0b6894187d12
K Lauer <klauer@bnl.gov>
2015-11-10 11:24:46 -0500

    Add gobject-introspection

---
+++ recipes/gobject-introspection/build.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# Copyright 2014-2015 Peter Williams and collaborators.
+# This file is licensed under a 3-clause BSD license; see LICENSE.txt.
+
+set -e
+# don't get locally installed pkg-config entries:
+export PKG_CONFIG_LIBDIR="$PREFIX/lib/pkgconfig"
+export PYTHON="$PREFIX/bin/python3.4"
+
+# conda libffi doesn't come with pkgconfig info:
+export FFI_CFLAGS="-I$PREFIX/include" FFI_LIBS="-L$PREFIX/lib"
+./configure --prefix=$PREFIX  # || { cat config.log ; exit 1 ; }
+make -j$(nproc --ignore=4)
+make install
+
+cd $PREFIX
+# workaround for libffi .la file path issues
+sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la lib/gobject-introspection/giscanner/*.la
