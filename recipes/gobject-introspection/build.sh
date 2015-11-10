66a5665c52ea285dbb36e1a7709fe7c0fc70b70f
K Lauer <klauer@bnl.gov>
2015-11-10 12:05:50 -0500

    Add (de)activate scripts for typelib path

--- recipes/gobject-introspection/build.sh
+++ recipes/gobject-introspection/build.sh
@@ -16,3 +16,23 @@ make install
 cd $PREFIX
 # workaround for libffi .la file path issues
 sed -i -e 's|lib/\.\./lib64|lib|g' lib/*.la lib/gobject-introspection/giscanner/*.la
+
+# deal with env export
+mkdir -p $PREFIX/etc/conda/activate.d
+mkdir -p $PREFIX/etc/conda/deactivate.d
+
+ACTIVATE=$PREFIX/etc/conda/activate.d/gobject_introspection.sh
+DEACTIVATE=$PREFIX/etc/conda/deactivate.d/gobject_introspection.sh
+
+GI_TYPELIB_PATH=$PREFIX/lib/girepository-1.0
+
+# set up
+echo "export GI_TYPELIB_PATH="$GI_TYPELIB_PATH >> $ACTIVATE
+
+# tear down
+echo "unset GI_TYPELIB_PATH" >> $DEACTIVATE
+
+# clean up after self
+unset ACTIVATE
+unset DEACTIVATE
+unset GI_TYPELIB_PATH
